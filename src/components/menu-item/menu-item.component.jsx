87d29dfb4e48361c27034d4d3de572283f5b9057
GokulRG <ramakrishnan.go@husky.neu.edu>
2019-07-31 05:41:32 -0700

    Menu Item Styling
    Finalized the styling on the Menu Item component.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,7 +6,8 @@ const MenuItem = (props) => {
     const { title, imageUrl, size } = props;
 
     return (
-        <div style={{backgroundImage:`url(${imageUrl})`}} className={`menu-item ${size}`}>
+        <div className={`menu-item ${size}`}>
+            <div className='background-image' style={{ backgroundImage: `url(${imageUrl})`, height: '100%', width: '100%' }} />
             <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'>SHOP NOW</span>
