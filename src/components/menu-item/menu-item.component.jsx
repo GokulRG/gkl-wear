21cfe40e91cbd0473eb323a0f8c6063fb7ca0021
GokulRG <ramakrishnan.go@husky.neu.edu>
2019-08-02 07:10:00 -0700

    Hats Page Route Adding
    Using withRouter and configuring the Route for the HatsPage.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
 const MenuItem = (props) => {
 
-    const { title, imageUrl, size } = props;
+    const { title, imageUrl, size, history, match, linkUrl } = props;
 
     return (
-        <div className={`menu-item ${size}`}>
+        <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className='background-image' style={{ backgroundImage: `url(${imageUrl})`, height: '100%', width: '100%' }} />
             <div className='content'>
                 <h1 className='title'>{title}</h1>
@@ -16,4 +18,4 @@ const MenuItem = (props) => {
     );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
