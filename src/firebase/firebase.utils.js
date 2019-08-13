790a32c89fadb9913f4cc7b429a785abc6387fb9
GokulRG <ramakrishnan.go@husky.neu.edu>
2019-08-13 06:20:43 -0700

    Saving Users to FireStore DB
    Implementing the functionality to save users to the Database.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,35 @@ const config = {
     appId: "1:439441474296:web:7093a0fdc926a009"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) {
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    //We're using await here because the actual get call to the DB is an asynchronous call
+    const userSnapshot = await userRef.get();
+
+    if (!userSnapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('Error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
