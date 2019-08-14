6cb8a31774b2dd6b51d0ba98e65d203f0b13b842
GokulRG <ramakrishnan.go@husky.neu.edu>
2019-08-14 06:03:01 -0700

    Finishing SignIn functionality
    Finishing SignIn functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
@@ -8,9 +8,17 @@ class SignIn extends React.Component {
 
     state = { email: '', password: '' };
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
-        this.setState({ email: '', password: '' })
+        
+        const { email, password } = this.state;
+        
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     handleChange = (event) => {
