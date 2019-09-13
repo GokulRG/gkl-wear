a38e234456446c812ae275d3d6b1f814f3cd5686
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-09-13 05:58:16 -0700

    Adding Cart Item Component
    Adding CartItem Component with styles

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => {
+const CartDropdown = (props) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items'></div>
+            <div className='cart-items'>
+                {
+                    props.cartItems.map(cartItem => {
+                        return <CartItem key={cartItem.id} item={cartItem} />
+                    })
+                }
+            </div>
             <CustomButton>CHECKOUT</CustomButton>
         </div>
     );
 };
 
-export default CartDropdown;
+const mapStateToProps = (state) => {
+    return { cartItems: state.cart.cartItems };
+}
+
+export default connect(mapStateToProps)(CartDropdown);
+
+//Here's how you get data from the state when you're using Redux
+//import connect from react-redux
+//Then call map State To Props, this function takes an input of the current state, take whatever you want from it and return that as an object with a property. That particular property should now be available in the props for you
+//Don't forget to wire the connect function during export default component.
+//Use props to display your state data
